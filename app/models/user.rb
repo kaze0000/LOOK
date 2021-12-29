fb2e3dd87b54e86d199bb18a7d7a9a56389a5763
kaze0000 <kazuya.00000.ds25@gmail.com>
2021-12-29 21:21:21 +0900

    Add: ログイン機能の実装とuserモデルのvalidate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :clothes, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum:255 }
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
 end
