f990fae95e17daaa427c1fe739962a8a938bfb9b
kaze0000 <kazuya.00000.ds25@gmail.com>
2021-12-29 22:41:23 +0900

    Fix: clothモデルの修正

--- app/models/brand_name.rb
+++ app/models/brand_name.rb
@@ -1,3 +1,5 @@
 class BrandName < ApplicationRecord
   has_many :brand_names, dependent: :destroy
+
+  validates :name, presence: true
 end
--- app/models/clothe.rb
+++ app/models/clothe.rb
@@ -1,4 +1,7 @@
 class Clothe < ApplicationRecord
   belongs_to :user
   belongs_to :brandname
+
+  enum type: { tops: 0, outer: 1, bottoms: 2 }
+  enum gender: { mens: 0, womens: 1 }
 end
