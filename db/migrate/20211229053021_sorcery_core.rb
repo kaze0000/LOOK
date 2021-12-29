34cd2dbe1b41c59117dd0bac9c7c04ef20d29f01
kaze0000 <kazuya.00000.ds25@gmail.com>
2021-12-29 14:44:04 +0900

    usersテーブルの作成

--- db/migrate/20211229053021_sorcery_core.rb
+++ db/migrate/20211229053021_sorcery_core.rb
@@ -1,7 +1,9 @@
 class SorceryCore < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :email,            null: false
+      t.string :name, null: false
+      t.integer :role, default: 0, null: false
+      t.string :email, null: false
       t.string :crypted_password
       t.string :salt
 
