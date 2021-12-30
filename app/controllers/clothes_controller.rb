f5b1f5771626e6de75476c495a9d048e959bb104
kaze0000 <kazuya.00000.ds25@gmail.com>
2021-12-30 19:01:57 +0900

    selectedの切り替えを実装(非同期化はまだ)

--- app/controllers/clothes_controller.rb
+++ app/controllers/clothes_controller.rb
@@ -3,4 +3,24 @@ class ClothesController < ApplicationController
     @clothes = Clothe.all
   end
 
+  def update
+    @clothe =  Clothe.find(params[:id])
+    # if  @clothe.selected == false
+    #   @clothe.update(selected: true)
+    # else
+    #   @clothe.update(selected: false)
+    # end
+    # redirect_to clothes_path
+    if @clothe.update(clothe_params)
+      redirect_to clothes_path
+    else
+      redirect_to clothes_path, notice: 'Error occurs'
+    end
+  end
+
+  private
+
+  def clothe_params
+    params.require(:clothe).permit(:selected)
+  end
 end
