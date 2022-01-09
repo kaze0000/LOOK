766521cad9611566d8476873db077cbfb2008d91
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-01-09 13:52:34 +0900

    myclosetの選択btnのバグ修正

--- app/controllers/myclosets_controller.rb
+++ app/controllers/myclosets_controller.rb
@@ -3,6 +3,7 @@ class MyclosetsController < ApplicationController
   before_action :set_q, only: [:index, :search]
   def index
     @my_clothes = current_user.clothes.includes(:brand_name).order(created_at: :desc).page(params[:page])
+    @selected_count = Clothe.where(selected: true, user_id: current_user.id).count
   end
 
   def new
@@ -53,6 +54,7 @@ class MyclosetsController < ApplicationController
   def select
     @clothe = Clothe.find(params[:id])
     @clothe.update(select_params)
+    @selected_count = Clothe.where(selected: true, user_id: current_user.id).count
   end
   
   private
