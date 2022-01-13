9a1d5bdb3efc5e3d5895e58dfb6b82f918c084bc
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-01-13 22:51:39 +0900

    shareclosetページの検索機能を修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,8 +3,8 @@ class StaticPagesController < ApplicationController
   after_action :reset
 
   def top
-      @selected_bottoms = Clothe.joins(:user_clothes).where(category: 2, user_clothes: {selected: true, user_id: current_user})
-      @selected_tops = Clothe.joins(:user_clothes).where(category: [0,1], user_clothes: {selected: true, user_id: current_user})
+    @selected_bottoms = Clothe.joins(:user_clothes).where(category: 2, user_clothes: {selected: true, user_id: current_user})
+    @selected_tops = Clothe.joins(:user_clothes).where(category: [0,1], user_clothes: {selected: true, user_id: current_user})
   end
 
   def reset
