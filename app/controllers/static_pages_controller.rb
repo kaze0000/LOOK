44b5364bc51220a9553b065d0db24c4e3c8d8575
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-01-13 22:35:26 +0900

    #resetがcurrent_userにのみ適応されるように修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,13 +3,11 @@ class StaticPagesController < ApplicationController
   after_action :reset
 
   def top
-    if current_user
-      @selected_bottoms = Clothe.joins(:user_clothes).where(category: 2, user_clothes: {selected: true})
-      @selected_tops = Clothe.joins(:user_clothes).where(category: [0,1], user_clothes: {selected: true})
-    end
+      @selected_bottoms = Clothe.joins(:user_clothes).where(category: 2, user_clothes: {selected: true, user_id: current_user})
+      @selected_tops = Clothe.joins(:user_clothes).where(category: [0,1], user_clothes: {selected: true, user_id: current_user})
   end
 
   def reset
-    UserClothe.includes(:user, :clothe).where(selected: true).update(selected: false)
+    UserClothe.includes(:user, :clothe).where(selected: true, user_id: current_user).update(selected: false)
   end
 end
