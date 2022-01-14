4279dd5854a69fbd8f5aea1836ee50c044b64577
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-01-14 22:24:11 +0900

    S3の設定を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,11 +6,13 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = 'fog/aws'
   config.fog_directory  = 'look-backet'
+  config.asset_host = 'https://s3.amazonaws.com/look-backet'
+  config.fog_public = true
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'ap-northeast-1',
+    region: ENV['AWS_DEFAULT_REGION'],
     path_style: true
   }
 end 
