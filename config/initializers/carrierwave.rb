3026a36b8ef32359dede5b5878bbbb12846824ea
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-01-15 09:39:59 +0900

    carrierwaveのローカル設定を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,16 +3,21 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory  = 'look-backet'
-  config.asset_host = 'https://s3.amazonaws.com/look-backet'
-  config.fog_public = true
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: ENV['AWS_DEFAULT_REGION'],
-    path_style: true
-  }
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'look-backet'
+    config.asset_host = 'https://s3.amazonaws.com/look-backet'
+    config.fog_public = true
+    config.cache_storage = :fog 
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_DEFAULT_REGION'],
+      path_style: true
+    }
+  else
+    config.storage = :file
+  end
 end 
