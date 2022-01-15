e95e4f6c5055792f9af31edd75674e1f02d9b5c5
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-01-15 11:05:02 +0900

    S3の設定

--- app/uploaders/clothe_image_uploader.rb
+++ app/uploaders/clothe_image_uploader.rb
@@ -7,7 +7,7 @@ class ClotheImageUploader < CarrierWave::Uploader::Base
   if Rails.env.production?
     storage :fog
   else
-    storage :fog
+    storage :file
   end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
