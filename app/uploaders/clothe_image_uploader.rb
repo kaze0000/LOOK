aecdfffd2733c3d284ae893b94a52739c5b5a09c
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-01-17 16:06:14 +0900

    Fix: storage:fogの削除

--- app/uploaders/clothe_image_uploader.rb
+++ app/uploaders/clothe_image_uploader.rb
@@ -4,11 +4,6 @@ class ClotheImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_and_pad: [300, 300, "rgba(255,255,255,0)", "Center"]
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
