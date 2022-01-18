e5b4fad9d89e9bb4912b39c57c91e56b464c919d
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-01-18 10:46:34 +0900

    controllerのリファクタリング

--- app/controllers/myclosets_controller.rb
+++ app/controllers/myclosets_controller.rb
@@ -1,8 +1,9 @@
 class MyclosetsController < ApplicationController
-  before_action :set_q, only: [:search]
+  before_action :set_q, only: %i[search]
+  before_action :set_user_clothe, only: %i[edit destroy select publish]
   def index
     @user_clothes = current_user.user_clothes.includes(:clothe, clothe: [:brand_name]).where.not(relation: 1, state: 1).order(created_at: :desc).page(params[:page])
-    @selected_count = UserClothe.where(selected: true, user_id: current_user.id).count
+    @selected_count = UserClothe.selected(current_user).count
   end
 
   def new
@@ -14,7 +15,7 @@ class MyclosetsController < ApplicationController
     @brand_name = BrandName.new(brand_name_params)
     if @brand_name.save
       clothe = @brand_name.clothe
-      @user_clothe = UserClothe.new(user_id: current_user.id, clothe_id: clothe.id)
+      @user_clothe = UserClothe.new(user_id: current_user, clothe_id: clothe)
       @user_clothe.save
       # 背景削除の処理
       result = RemoveBg.from_file("public/#{clothe.image.url}")
@@ -35,8 +36,11 @@ class MyclosetsController < ApplicationController
   end
   
   def edit
-    @user_clothe = UserClothe.find(params[:id])
     @brand_name = @user_clothe.clothe.brand_name
+  end
+  
+  def destroy
+    @user_clothe.destroy!
   end
 
   def update
@@ -45,25 +49,18 @@ class MyclosetsController < ApplicationController
     @brand_name.update(update_brand_name_params)
   end
 
-  def destroy
-    @user_clothe = UserClothe.find(params[:id])
-    @user_clothe.destroy!
-  end
-
   def search
-    @results = @q.result.includes(:clothe, clothe: [:brand_name]).where(user_clothes: {user_id: current_user.id})
-    @selected_count = UserClothe.where(selected: true, user_id: current_user.id).count
+    @results = @q.result.includes(:clothe, clothe: [:brand_name]).where(user_clothes: {user_id: current_user})
+    @selected_count = UserClothe.selected(current_user).count
   end
 
   def select
-    @user_clothe = UserClothe.find(params[:id])
     @user_clothe.update(select_params)
-    @selected_count = UserClothe.where(selected: true, user_id: current_user.id).count
+    @selected_count = UserClothe.selected(current_user).count
   end
 
   def publish
-    @clothe = Clothe.find(params[:id])
-    @clothe.user_clothes.update(publish_params)
+    @user_clothe.update(publish_params)
     redirect_to myclosets_path
   end
   
@@ -71,6 +68,10 @@ class MyclosetsController < ApplicationController
   
   def set_q
     @q = UserClothe.ransack(params[:q])
+  end
+
+  def set_user_clothe
+    @user_clothe = UserClothe.find(params[:id])
   end
   
   def select_params
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,12 @@
 class StaticPagesController < ApplicationController
   skip_before_action :require_login
   after_action :reset
-
   def top
-    @selected_bottoms = Clothe.joins(:user_clothes).where(category: 2, user_clothes: {selected: true, user_id: current_user})
-    @selected_tops = Clothe.joins(:user_clothes).where(category: [0,1], user_clothes: {selected: true, user_id: current_user})
+    @selected_bottoms = Clothe.joins(:user_clothes).where(category: 2, user_clothes: { selected: true, user_id: current_user })
+    @selected_tops = Clothe.joins(:user_clothes).where(category: [0,1], user_clothes: { selected: true, user_id: current_user })
   end
 
   def reset
-    UserClothe.includes(:user, :clothe).where(selected: true, user_id: current_user).update(selected: false)
+    UserClothe.includes(:user, :clothe).selected(current_user).update(selected: false)
   end
 end
