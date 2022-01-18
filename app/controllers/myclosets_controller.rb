aa998edf24f9910f920188dad3b6b16829fdaba3
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-01-18 15:15:34 +0900

    controllerのリファクタリング

--- app/controllers/myclosets_controller.rb
+++ app/controllers/myclosets_controller.rb
@@ -1,8 +1,9 @@
 class MyclosetsController < ApplicationController
   before_action :set_q, only: %i[search]
-  before_action :set_user_clothe, only: %i[edit destroy select publish]
+  before_action :set_user_clothe, only: %i[edit destroy select]
+  before_action :set_clothe, only: %i[update publish]
   def index
-    @user_clothes = current_user.user_clothes.includes(:clothe, clothe: [:brand_name]).where.not(relation: 1, state: 1).order(created_at: :desc).page(params[:page])
+    @user_clothes = current_user.user_clothes.includes(:clothe, clothe: [:brand_name]).mine_and_others_published.order(created_at: :desc).page(params[:page])
     @selected_count = UserClothe.selected(current_user).count
   end
 
@@ -15,12 +16,11 @@ class MyclosetsController < ApplicationController
     @brand_name = BrandName.new(brand_name_params)
     if @brand_name.save
       clothe = @brand_name.clothe
-      @user_clothe = UserClothe.new(user_id: current_user, clothe_id: clothe)
-      @user_clothe.save
+      user_clothe = UserClothe.new(user_id: current_user.id, clothe_id: clothe.id)
+      user_clothe.save
       # 背景削除の処理
       result = RemoveBg.from_file("public/#{clothe.image.url}")
       result.save("public/#{clothe.image.url}", overwrite: true)
-      
       # s3に保存
       file = File.new("public/#{clothe.image.url}")
       s3 = Aws::S3::Resource.new
@@ -44,13 +44,12 @@ class MyclosetsController < ApplicationController
   end
 
   def update
-    @clothe = Clothe.find(params[:id])
     @brand_name = @clothe.brand_name
     @brand_name.update(update_brand_name_params)
   end
 
   def search
-    @results = @q.result.includes(:clothe, clothe: [:brand_name]).where(user_clothes: {user_id: current_user})
+    @results = @q.result.includes(:clothe, clothe: [:brand_name]).where(user_clothes: {user_id: current_user.id})
     @selected_count = UserClothe.selected(current_user).count
   end
 
@@ -60,7 +59,7 @@ class MyclosetsController < ApplicationController
   end
 
   def publish
-    @user_clothe.update(publish_params)
+    clothe.user_clothes.update(publish_params)
     redirect_to myclosets_path
   end
   
@@ -72,6 +71,10 @@ class MyclosetsController < ApplicationController
 
   def set_user_clothe
     @user_clothe = UserClothe.find(params[:id])
+  end
+
+  def set_clothe
+    @clothe = Clothe.find(params[:id])
   end
   
   def select_params
@@ -87,6 +90,6 @@ class MyclosetsController < ApplicationController
   end
 
   def update_brand_name_params
-    params.require(:brand_name).permit(:id,:name, clothe_attributes:[:id, :category, :gender])
+    params.require(:brand_name).permit(:id, :name, clothe_attributes:[:id, :category, :gender])
   end
 end
--- app/models/user_clothe.rb
+++ app/models/user_clothe.rb
@@ -5,5 +5,8 @@ class UserClothe < ApplicationRecord
   enum relation: { original: 0, replica: 1 }
   enum state: { published: 0, privated: 1 }
 
-  scope :selected, -> (user){ where(selected: true, user_id: user) }
+  scope :selected, -> (user_id){ where(selected: true, user_id: user_id) }
+  scope :original, -> { where(relation: 0) }
+  scope :published, -> { where(state: 0) }
+  scope :mine_and_others_published, -> { where.not(relation: 1, state: 1 ) }
 end
