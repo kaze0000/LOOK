5821d2614b9bd52690c27455213addac6108b0f6
kaze0000 <kazuya.00000.ds25@gmail.com>
2022-01-18 17:38:26 +0900

    meta_tagの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,25 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'LOOK',
+      reverse: true,
+      separator: '|',
+      description: 'ブラウザ上で服の組み合わせが試せるサービス。',
+      keywords: '服,ファッション,コーディネート',
+      canonical: request.original_url,
+      noindex: ! Rails.env.production?,
+      icon: [                  
+        { href: image_url('favicon.png') },
+        { href: image_url('ogp.png'), rel: 'apple-touch-icon', sizes: '180x180', type: 'image/png' },
+      ],
+      og: {
+        site_name: 'LOOK',
+        description: 'ブラウザ上で服の組み合わせが試せるサービス。', 
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'),
+        locale: 'ja_JP',
+      },
+    }
+  end
 end
